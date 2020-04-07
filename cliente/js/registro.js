930b63ed494dd8cad15f9aac82d17e7409c69772
unknown <e.fraffo@gmail.com>
2020-04-07 20:54:12 -0300

    actualizar usuario y login - a terminar

--- cliente/js/registro.js
+++ cliente/js/registro.js
@@ -19,6 +19,29 @@ $('#new').click(function(event){
     });
 });
 
+$('#save').click(function(event){
+    event.preventDefault();
+    var server = 'http://localhost:8080';
+    var name = $('#exampleInputName1').val();
+    console.log(name)
+    var email = $('#exampleInputEmail1').val();
+    // var id = localStorage.getItem(localStorage.key(i));
+    var id = 4;
+
+    $.ajax({
+        url: server + "/usuario/"+id+"/actualizar",
+        method: 'put',
+        data: {"name": name, "email": email},
+        success: function(){
+            alert('Usuario actualizado con exito');
+            window.location.href = "index.html";
+        },
+        error: function(error){
+            window.location.href = "error.html";
+        }
+    });
+});
+
 // sgMail
 //   .send(msg)
 //   .then(() => {}, error => {
